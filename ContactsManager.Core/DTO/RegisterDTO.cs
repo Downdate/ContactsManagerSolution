d35d72706915b8eb45b8e571f1ff93d3425d7d70
Dan <Danielzareimain@gmail.com>
2026-07-21 11:22:21 +0330

    added confirm password compare validation.

--- ContactsManager.Core/DTO/RegisterDTO.cs
+++ ContactsManager.Core/DTO/RegisterDTO.cs
@@ -26,6 +26,7 @@ namespace ContactsManager.Core.DTO
 
         [Required(ErrorMessage = "Confirm Password can't be blank")]
         [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password and confirm password should be the same.")]
         public string ConfirmPassword { get; set; }
 
 
